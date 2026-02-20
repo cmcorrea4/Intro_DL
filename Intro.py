23f30c8b97ed15f152d8fe71791845146ff8c850
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2026-02-19 20:39:59 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.title("Mi primera APP interactiva")
+
+# Texto
+st.write("Esta es una interfaz muy sencilla.")
+
+# Entrada de texto
+#nombre = st.text_input("Escribe tu nombre:")
+
+# Botón
+#if st.button("Saludar"):
+#    st.success(f"Hola {nombre}, bienvenido a Streamlit 🚀")
+
+# Slider
+#edad = st.slider("Selecciona tu edad:", 0, 100, 25)
+#st.write("Tu edad es:", edad)
